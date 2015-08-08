0c0e18a97d03c4b3d62f4ca9931744b71ddd9b5f
richm39804git config --global user.email richmontero25@gmail.comgit config --global user.name richm39804 <richmontero25@gmail.com>
2015-08-08 08:32:43 -0700

    entry.js model author,photo_url,date_taken added

--- models/entry.js
+++ models/entry.js
@@ -1,1 +1,12 @@
 // Entry model
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var EntrySchema = new mongoose.Schema({
+  author: String,
+  photo_url: String,
+  date_taken: String,
+});
+
+var Entry = mongoose.model('Entry', EntrySchema);
+module.exports = Entry;
